04813972c5fe1ccdc4a66ac8d5201c56d06bfe52
developer1 <aansari4u@gmail.com>
2022-10-26 21:40:49 +0530

    refactor: revert to default

--- src/main/java/com/bits/devops/calculator/Calculator.java
+++ src/main/java/com/bits/devops/calculator/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator {
 
     private static final int WINDOW_WIDTH = 410;
     private static final int WINDOW_HEIGHT = 600;
-    private static final int BUTTON_WIDTH = 120;  //change from 80
+    private static final int BUTTON_WIDTH = 80;
     private static final int BUTTON_HEIGHT = 70;
     private static final int MARGIN_X = 20;
     private static final int MARGIN_Y = 60;
